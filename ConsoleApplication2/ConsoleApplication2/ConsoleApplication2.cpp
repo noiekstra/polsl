41540088e4930308eee3761938e3ca67ec10b27a
noiekstra <agata.pieczka@op.pl>
2019-11-06 11:30:20 +0100

    h

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string tekst;
+	int dl, ile = 0;
+
+	cout << "\n\tWczytaj linijke tekstu: ";
+	getline(cin, tekst);
+
+	dl = tekst.size();
+	for (int i = 0; i < dl; i++)
+	{
+		if (tekst[i] >= 'A'&&tekst[i] <= 'Z')
+			ile++;
+	}
+	cout << ile << endl;
+	return 0;
+}
---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int i, j;
+	string wyraz;
+
+	cout << "Podaj wyraz, ktory chcesz sprawdzic: ";
+	cin >> wyraz;
+	
+	for (i = 0, j = wyraz.length() - 1; i < j; i++, j--)
+	{
+		if (wyraz[i] != wyraz[j])
+			break;
+	}
+	if (i < j)
+		cout << "Podany wyraz nie jest palindromem" << endl;
+	else
+		cout << "Podany wyraz jest palindromem" << endl;
+
+	return 0;
+}
