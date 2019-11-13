df598a05f3a62a4d493b5b96adf574e0a6838d6e
noiekstra <agata.pieczka@gmail.com>
2019-11-13 11:41:10 +0100

    zmiany

---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool sprawdz(int tab[10], int n, int x);
+
+int main()
+{
+	for (i = 0; i < 10; i++)
+	{
+		tab[i] = rand % 10;
+
+
+	}
+
+
+	return 0;
+}
+
+bool sprawdz(int tab10], int n, int x);
+{
+
+
+}
+
+bool sprawdz(int tab[10], int n, int x)
+{
+	return false;
+}
