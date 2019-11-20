350e0643c92ac017fa589bc2f452fc3c92851a03
noiekstra <agata.pieczka@op.pl>
2019-11-20 11:41:16 +0100

    sqrt

---
+++ ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int fib(int n);
+int main()
+{
+	int n;
+	cout << "\n\tPodaj nr wyrazu ciagu: ";
+	cin >> n;
+	cout <<"\n\t"<< n << " wyraz ciagu ma wartosc " << fib(n) << endl;
+
+	system("pause");
+	return 0;
+}
+
+int fib(int n)
+{
+	if (n < 3)
+		return 1;
+	return fib(n - 2) + fib(n - 1);
+}
