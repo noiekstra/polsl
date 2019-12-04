32399d50c818a685c0ee4da80004cbd047a06bfe
noiekstra <agata.pieczka@op.pl>
2019-12-04 11:43:10 +0100

    blep

---
+++ ConsoleApplication9/ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include <iostream>
+#include<fstream> 
+#include <ctime>
+#include <algorithm>
+
+
+using namespace std;
+
+int main()
+{
+	ofstream plik_wy;
+	srand(time(0));
+	plik_wy.open("liczby.txt");
+	if (plik_wy.is_open())
+	{
+		for (int i = 0; i < 20; i++)
+			plik_wy << rand() % 100<<endl;
+			plik_wy.close();
+	}
+
+	ifstream plik_we;
+	plik_we.open("liczby.txt");
+	if (plik_we.is_open())
+	{
+		int tab[20];
+		for (int i = 0; i < 20; i++)
+			plik_we >> tab[i];
+		plik_we.close();
+		sort(tab, tab + 20);
+		for (int i = 0; i < 20; i++)
+			cout << tab[i] << endl;
+
+
+
+		int suma = 0;
+		for (int i = 0; i < 20; i++)
+		{
+			suma = suma + tab[i];
+		}
+		cout << "suma liczb, to: " << suma << endl;
+		system("pause");
+	}
+}
